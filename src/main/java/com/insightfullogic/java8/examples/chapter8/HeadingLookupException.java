d2173b385fd4fd5a8115fc88866d946e2661526c
piaolinzhi <piaolinzhi@163.com>
2017-02-08 22:59:59 +0800

    格式化

--- src/main/java/com/insightfullogic/java8/examples/chapter8/HeadingLookupException.java
+++ src/main/java/com/insightfullogic/java8/examples/chapter8/HeadingLookupException.java
@@ -3,6 +3,8 @@ package com.insightfullogic.java8.examples.chapter8;
 import java.io.IOException;
 
 public class HeadingLookupException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public HeadingLookupException(IOException e) {
     }
 }
